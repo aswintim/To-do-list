900ada96d4d23fa9786031a3152377a12a76aafd
Aswin Timalsina <aswin.timalsina1@gmail.com>
2019-12-20 08:59:25 +0545

    added done feature

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 var parser = require('body-parser');
+
 var mongoose = require('mongoose');
 
 app.use(parser.urlencoded({extended: true}));
@@ -28,7 +29,12 @@ var schem = new mongoose.Schema({
     name:String
 });
 
+//names model
 var mod = mongoose.model("name", schem);
+
+//dones model
+var done = mongoose.model("done", schem);
+
 
 app.get('/', function(req, res){
     mod.find({}, function(err, val){
@@ -37,7 +43,7 @@ app.get('/', function(req, res){
     })
 });
 
-app.post('/action', function(req, res){
+app.post('/submit', function(req, res){
     var user = new mod({
         name: req.body.todo
     });
@@ -47,6 +53,28 @@ app.post('/action', function(req, res){
             console.log('New item: '+ val.name);
         }
     })
+    res.redirect('/');
+})
+
+app.post('/done', function(req, res){
+    var request= req.body.done;
+    var query = {name: request
+    }
+    mod.remove(query, (err, obj)=>{
+        if(err){console.log(err)}
+        else{console.log("Document removed from main:"+obj)}
+    })
+    var done1 = new done({
+        name: request
+    })
+
+    done.create(done1, (err, val) => {
+        if(err){console.log(err)}
+        else{
+            console.log('New item added in dones: ' + val);
+        }
+    })
+    console.log(req.body.done);
     res.redirect('/');
 })
 
